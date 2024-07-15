ba8ec91776afd6f03e56c3bf334b0782a7943e77
Theo Addis <theocaddis@gmail.com>
2024-07-15 12:21:29 +0100

    Completed ./exercises/036_enums2.zig

--- exercises/036_enums2.zig
+++ exercises/036_enums2.zig
@@ -28,11 +28,7 @@ const std = @import("std");
 //     #RRGGBB
 //
 // Please define and use a pure blue value Color:
-const Color = enum(u32) {
-    red = 0xff0000,
-    green = 0x00ff00,
-    blue = ???,
-};
+const Color = enum(u32) { red = 0xff0000, green = 0x00ff00, blue = 0x0000ff };
 
 pub fn main() void {
     // Remember Zig's multi-line strings? Here they are again.
@@ -49,16 +45,17 @@ pub fn main() void {
     // Please add this formatting to the blue value.
     // (Even better, experiment without it, or try parts of it
     // to see what prints!)
+
     std.debug.print(
         \\<p>
         \\  <span style="color: #{x:0>6}">Red</span>
         \\  <span style="color: #{x:0>6}">Green</span>
-        \\  <span style="color: #{}">Blue</span>
+        \\  <span style="color: #{x:0>6}">Blue</span>
         \\</p>
         \\
     , .{
         @intFromEnum(Color.red),
         @intFromEnum(Color.green),
-        @intFromEnum(???), // Oops! We're missing something!
+        @intFromEnum(Color.blue),
     });
 }
